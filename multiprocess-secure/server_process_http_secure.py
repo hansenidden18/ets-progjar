c0eab7d3bcd27b8f1864349695c6480b77ab9d1e
hansenidden18 <hansenidden@gmail.com>
2023-03-30 18:02:43 +0700

    Done

--- multiprocess-secure/server_process_http_secure.py
+++ multiprocess-secure/server_process_http_secure.py
@@ -13,11 +13,11 @@ from http import HttpServer
 httpserver = HttpServer()
 
 
-class ProcessTheClient(multiprocessing.Process):
+class ProcessTheClient(threading.Thread):
 	def __init__(self, connection, address):
 		self.connection = connection
 		self.address = address
-		multiprocessing.Process.__init__(self)
+		threading.Thread.__init__(self)
 
 	def run(self):
 		rcv=""
@@ -49,7 +49,7 @@ class ProcessTheClient(multiprocessing.Process):
 
 
 
-class Server(threading.Thread):
+class Server(multiprocessing.Process):
 	def __init__(self,hostname='testing.net'):
 		self.the_clients = []
 #------------------------------
@@ -61,10 +61,10 @@ class Server(threading.Thread):
 #---------------------------------
 		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		threading.Thread.__init__(self)
+		multiprocessing.Process.__init__(self)
 
 	def run(self):
-		self.my_socket.bind(('0.0.0.0', 8443))
+		self.my_socket.bind(('0.0.0.0', 9443))
 		self.my_socket.listen(1)
 		while True:
 			self.connection, self.client_address = self.my_socket.accept()
